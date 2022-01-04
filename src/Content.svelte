9fe2e677f07edfe68047950034918d4edfdcb24a
Gnorme <thompsoncwt@gmail.com>
2022-01-04 15:47:12 -0500

    Add files via upload

--- src/Content.svelte
+++ src/Content.svelte
@@ -5,63 +5,15 @@
     import VoteToken from './VoteToken.svelte';
     import Roadmap from './Roadmap.svelte';
     import Carousel from './Carousel.svelte';
+    import Table from './Table.svelte';
 
-	const images = [
-		{path: 'images/bass.avif', id: 'image1'},
-		{path: 'images/Dunk.avif', id: 'image2'},
-		{path: 'images/skateboard.avif', id: 'image3'},
-		{path: 'images/runner.avif', id: 'image4'},
-		// {path: 'images/image6.jpg', id: 'image6'},
-	]
 </script>
 
-<!-- 
- <div id="Trailer" class="content">
-    <div class="card"><img src="images/bass.jpg" /></div>
-    <div class="card"><img src="images/Dunk.jpg" /></div>
-    <div class="card"><img src="images/skateboard.jpg" /></div>
-    <div class="card"><img src="images/runner.jpg" /></div>
-</div>
--->
-
-    <Carousel />
-    <!--<Carousel 
-        {images}
-        imageWidth={350}
-        imageSpacing={15}
-        controlColor={'white'}
-        controlScale={0.8}
-        displayControls={true}
-        autoplay={true}
-        autoplaySpeed={3000}
-    />-->
-
-
+<Carousel />
 
 <div class="text-content">
     <h2 style="margin-left:7%;">What We Provide</h2>
-    <div class="table-content">
-        <div class="grid-item">
-            <div class="table-icon"><img alt="Handshake" style="width:100%" src="images/handshake.png"></div>
-            <h3>Skills Competition</h3>
-            <p>Using our platform, creators can challenge others using a video showcasing their talents. Challenge winners are decided by a community vote and all votes enter a reward pool that gets distrubuted to the winning creator and all those who voted for it to win.</p>
-        </div>
-        <div class="grid-item">
-            <div class="table-icon"><img alt="Earnings" style="width:100%" src="images/earn.png"></div>
-            <h3>Earning Potential</h3>
-            <p>Our carefully crafted royalties system and stablecoin Vote token will allow both creators and consumers of content to earn money. Creators can earn by winning challenges and viewers can earn by voting.</p>
-        </div>
-        <div class="grid-item">
-            <div class="table-icon"><img alt="Collectibles" style="width:100%" src="images/stamps.png"></div>
-            <h3>NFT Collectibles</h3>
-            <p>Collect unique moments from talented creators. Winning challenge videos are minted into NFT collectibles that can be traded on our NFT marketplace.</p>
-        </div>
-        <div class="grid-item">
-            <div class="table-icon"><img alt="Marketplace" style="width:100%" src="images/auction.png"></div>
-            <h3>NFT Marketplace</h3>
-            <p>Buy and Sell winning challenge moments on our NFT Marketplace to increase your collection. Royalties from collectible sales are distributed to the video creator and voters.</p>
-        </div>
-    </div>
+    <Table />
     <div class="powered-by">
         <h3><span style="color:aqua;">AllSkills</span> is powered by cryptocurrency, using <span style="background-color:aqua; color: rgb(30,30,30);">future-proof blockchain & smart contract technology</span> to connect talented people and audiences, and foster skill development through competition, all while providing <span style="background-color: aqua; color: rgb(30,30,30);">incentives for everyone involved.</h3>
     </div>
@@ -100,31 +52,6 @@
     padding-left:7%;
     padding-right:7%;
 }
-    p {
-        font-size: 1rem;
-        line-height: 1.5rem;
-        font-weight:100;
-        font-family: "BentonSans";
-        padding-top:2.5rem;
-        margin-block-start:0;
-        grid-area: c;
-    }
-    .table-icon {
-        align-items: center;
-        grid-area:a;
-        display:grid;
-        padding-right:2rem;
-    }
-    h3 {
-        font-size: 3rem;
-        line-height: 1.5;
-        margin-top:0;
-        margin-block-start:0;
-        margin-block-end:0;
-        font-weight: 500;
-        font-family: "Oswald";
-        grid-area: b;
-    }
     .powered-by{
         margin-top: 20rem;
         margin-bottom: 20rem;
@@ -132,49 +59,15 @@
         padding-left:7%;
         padding-right:7%;
         text-align: center;
-    }
-    .grid-item{
-        display:grid;
-        grid-template:
-            "a b b"
-            "a c c"
-            "a c c";
-        padding: 2rem;
-        border: 3px solid aqua;
-    }
-    .table-content{
-        display:grid;
-        margin-left:10%;
-        margin-right:10%;
-        margin-top:3rem;
-        margin-bottom:3rem;
-        color: white;
-        border: 3px solid aqua;
-        grid-template-columns: repeat(2,minmax(0,1fr));
+        font-size: 2.5rem;
+        font-family: "Oswald";
     }
     @media screen and (max-width: 820px) {
-        .table-content {
-            display:block;
-            border: 2px solid aqua;
-        }
         h3 {
-            font-size: 2.5rem;
+            font-size: 2.5rem !important;
         }
         h2 {
             text-align:center !important;
-        }
-        .grid-item{
-            grid-template: 
-                "a b b"
-                "c c c"
-                "c c c";
-            padding: 1.25rem;
-            border: 2px solid aqua;
-        }
-        .table-icon {
-            padding-right:1.25rem;
-            height:100px;
-            min-width: 50px;
         }
     }
     h2 {
