b2dba3d7ea501fe457a45ecfcefc21035bae7291
Gnorme <thompsoncwt@gmail.com>
2022-01-04 17:51:54 -0500

    Add files via upload

--- src/Challenge.svelte
+++ src/Challenge.svelte
@@ -5,7 +5,7 @@
         </div>
         <div class="content second">
             <h3 style="border-bottom: 2px solid aqua;">Take videos showcasing your skills</h3>
-            <p>No matter if your skill is athletic, artistic, musical,  strategic,  incredibly precise, or even one-of-a-kind, we want to see it. Setup your camera up to capture your finest moment and use our custom editing features to make it stand out even more.</p>
+            <p>No matter if your skill is  <span style="color:aqua;">athletic, artistic, musical,  strategic,  incredibly precise, or even one-of-a-kind,</span> we want to see it. Setup your camera up to capture your finest moment and use our custom editing features to make it stand out even more.</p>
         </div>
     </div>
     <div class="section">
@@ -14,7 +14,7 @@
         </div>
         <div class="content first">
             <h3 style="border-bottom: 2px solid red;">Put videos up for Challenge</h3>
-            <p>After you’re satisfied with your video, it’s time to put it up for Challenge. Winning a challenge round is how your video becomes eligible for minting. Choose between putting your video up for Open Challenge or directly challenging another video or user. Round lengths can be either 24h or 72h.</p>
+            <p>After you’re satisfied with your video, it’s time to put it up for Challenge.  <span style="color:red;">Winning a challenge round is how your video becomes eligible for minting.</span> Choose between putting your video up for Open Challenge or directly challenging another video or user. Round lengths can be either 24h or 72h.</p>
         </div>
     </div>
     <div class="section">
@@ -22,8 +22,8 @@
             <img src="" />
         </div>
         <div class="content second">
-            <h3 style="border-bottom: 2px solid purple;">Users vote to determine winner</h3>
-            <p>Over the duration of the challenge, users can use Vote tokens to cast their vote for which video they like most. Once the challenge is complete, the video with the most votes is declared the winner and minted as an NFT.</p>
+            <h3 style="border-bottom: 2px solid blueviolet;">Users vote to determine winner</h3>
+            <p>Once the challenge starts, <span style="color:blueviolet;">the control is now in the voters hands.</span> Users can use Vote tokens to show their support and become part of the journey. Once the challenge is complete, the video with the most votes is declared the winner and minted as an NFT.</p>
         </div>
     </div>
     <div class="section">
@@ -32,7 +32,7 @@
         </div>
         <div class="content first">
             <h3 style="border-bottom: 2px solid gold;">Earn money from voting and winning challenges</h3>
-            <p>Get rewarded for showing off your skills. The creator of the winning video will receive between 1-25% of all vote tokens cast during the challenge while the rest is rewarded to all those who voted for the winning video. The creators cut changes based on how close the vote is. Closer vote count = bigger cut.</p>
+            <p>Get rewarded for showing off your skills. The creator of the winning video will receive between 1-25% of all vote tokens cast during the challenge while the rest is rewarded to all those who voted for the winning video. The creators cut changes based on how close the vote is. <span style="color: gold;">Closer vote count = bigger cut.</span></p>
         </div>
     </div>
     <div class="video-container">
