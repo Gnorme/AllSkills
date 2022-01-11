bfd02544b9298e849b47de5d959502fad733bd20
Gnorme <thompsoncwt@gmail.com>
2022-01-11 07:03:04 -0500

    Add files via upload

--- src/VoteToken.svelte
+++ src/VoteToken.svelte
@@ -1,61 +1,61 @@
 <div class="vote-container">
-    <div class="column">
-       <div class="section">
+       <div class="section left">
            <span class="title">
             <img alt="Vote" src="images/vote.png" />
                 <h3>Cast your vote</h3>
             </span>
            <p>Show your support by voting for the challenge video you like most. All votes form a <span class="highlight">reward pool</span> and decide the winning video.</p>
        </div>
-       <div class="section">
+       <div class="section left">
         <span class="title">
             <img alt="Earnings" src="images/earnings.png" />
              <h3>Earn through voting</h3>
          </span>
         <p>The reward pool is split between the winner and all those who voted for them. <span class="highlight">Earn up to 145%</span> back for votes cast on winning videos.</p>
     </div>
-    <div class="section">
+    <div class="section left">
         <span class="title">
             <img alt="Royalties" src="images/revenue.png" />
              <h3>Share in the success</h3>
          </span>
         <p>Voters continue to share in the success by <span class="highlight">earning royalties</span> everytime a video they voted for is sold on any marketplace.</p>
     </div>
-    </div>
     <div class="column middle">
         <h1 id="value">1 $Vote ≈ $0.10</h1>
-        <img alt="Vote Token" src="images/token.png" />
+        <img id="token" alt="Vote Token" src="images/token.png" />
     </div>
-    <div class="column">
-        <div class="section">
+        <div class="section right">
             <span class="title">
                 <img alt="Stable value" src="images/stable.png" />
                  <h3>Redeemable at stable value</h3>
              </span>
             <p>$Vote tokens are <span class="highlight">backed by fiat reserves</span> at 1:1 ratio and will always be redeemable at a stable value.</p>
         </div>
-        <div class="section">
+        <div class="section right">
             <span class="title">
                 <img alt="Stake" src="images/stake.png" />
                  <h3>Stake to earn more</h3>
              </span>
             <p>Staking your $Vote tokens allows you to <span class="highlight">earn $Skills tokens.</span> Use $Skills tokens to participate in raffles and to voice your opinion.</p>
         </div>
-        <div class="section">
+        <div class="section right">
             <span class="title">
                 <img alt="Replenish" src="images/replenish.png" />
                  <h3>Easily replenish your wallet</h3>
              </span>
              <p>If you run out of Vote tokens, you can <span class="highlight">watch sponsored videos to earn more</span> and get right back in the game. </p>
         </div>
-    </div>
 </div>
 
 <style>
+    #token {
+        grid-area: token;
+    }
     #value{
         font-family: 'Ultra'; 
         font-family: 'Asphaltic'; 
         font-family: 'Oswald';
+        grid-area: value;
     }
     .highlight {
         color: aqua;
@@ -74,6 +74,9 @@
         flex-direction: column;
         align-items:center;
         justify-content: center;
+        grid-area:middle;
+        margin-left:auto;
+        margin-right:auto;
     }
     h3 {
        margin:0;
@@ -100,29 +103,35 @@
         display: inline-flex;
         align-items: center;
     }
-    .section {
-        height: 33%;
-        width: 100%;
-    }
     h1 {
         color: aqua;
         font: "Lato";
     }
     .column {
-        width: 25%;
+        width: 80%;
     }
     .vote-container{
-        display:flex;
-        height: 75vh;
-        max-height: 600px;
+        display:grid;
         min-height:525px;
         margin-bottom:300px;
         margin-left:auto;
         margin-right:auto;
+        grid-template-rows: 33% !important;
+        grid-template-columns: 1fr 1.25fr 1fr !important;
         justify-content: space-evenly;
+        grid-template:
+            "left middle right"
+            "left middle right"
+            "left middle right";
+    }
+    @media screen and (min-width: 1920px) {
+        p {
+            font-size: 1rem !important;
+        }
     }
     @media screen and (max-width: 820px) {
         .vote-container {
+            display:flex;
             flex-direction: column;
             height: auto;
             max-height: 100%;
