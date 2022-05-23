f8aab154e7bb0e3f17b1b80c258975edbcaa7418
Gnorme <thompsoncwt@gmail.com>
2022-05-23 06:49:00 -0400

    Add files via upload

--- src/routes/links.svelte
+++ src/routes/links.svelte
@@ -1,1 +1,29 @@
-<div>hello</div>
+<script context="module">
+  export const prerender = true;
+</script>
+
+<script>
+  import { t } from "$lib/translations";
+</script>
+
+<svelte:head>
+  <title
+    >AllSkills - Competition based NFT Platform for showcasing all skills.</title
+  >
+  <meta
+    name="description"
+    content="AllSkills is a competition based NFT platform where both talented creators and consumers of content can earn money."
+  />
+  <style>
+  </style>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400&family=Roboto:wght@100&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+<div>Hello</div>
+
+<style>
+</style>
